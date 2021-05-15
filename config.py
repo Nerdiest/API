adc255fa41ba37f0180538c2e25f468502e6cd5d
Aske Merci <rainman1011@tuta.io>
2021-05-15 19:04:54 +0200

    config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
-../config.py
+server = dict(
+    calculatorsPath='/home/vagrant/code/nerdiest/Calculators/',
+    host='api.nerdiest.io',
+    debug=True
+)
+limits = dict(
+    chudnovsky=500010,
+    fibonacci=500
+)
