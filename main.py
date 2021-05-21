2014ea1b2a7a80c7d29dea60911eea87dc92579e
rainman <rainman1011@tuta.io>
2021-05-21 10:10:22 +0200

    cors fix

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from flask_cors import CORS, cross_origin
 import config
 app = Flask(__name__)
 app.config["DEBUG"] = config.server["debug"]
-CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, resources={r"/v1/tools": {"origins": "https://nerdiest.io"}})
+
 
 @app.route('/v1/tools', methods=['POST'])
+@cross_origin(origin='https://nerdiest.io',headers=['Content- Type','Authorization'])
 def handle():
     from handler import Handler
     data = request.get_json()
@@ -24,4 +28,4 @@ def home():
     return "<img src='https://media4.giphy.com/media/8abAbOrQ9rvLG/200.gif' style='width:100%'>"
 
 
-app.run(host=config.server['host'], ssl_context='adhoc')
+app.run(host=config.server['host'], ssl_context=('certificate.crt', 'private.key'))
