e8cb737a5b3d298d6d2cd6697b366f4b4146fab5
Aske Merci <rainman1011@tuta.io>
2021-08-10 19:07:26 +0200

    closes #2 #3 #4 #5 #6

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ cors = CORS(app, resources={r"/v1/tools": {"origins": "https://nerdiest.io"}})
 
 
 @app.route('/v1/tools', methods=['POST'])
-@cross_origin(origin='https://nerdiest.io',headers=['Content- Type','Authorization'])
+@cross_origin(
+    origin='https://nerdiest.io',
+    headers=['Content- Type', 'Authorization'])
+
 def handle():
     from handler import Handler
     data = request.get_json()
@@ -22,10 +25,10 @@ def handle():
     res = Handler(call, type, params)
     return res.handle()
 
-
-@app.route('/', methods=['GET'])
-def home():
-    return "<img src='https://media4.giphy.com/media/8abAbOrQ9rvLG/200.gif' style='width:100%'>"
-
-
-app.run(host=config.server['host'], ssl_context=('certificate.crt', 'private.key'))
+app.run(
+    host=config.server['host'],
+    ssl_context=(
+        'certificate.crt',
+        'private.key'
+        )
+    )
